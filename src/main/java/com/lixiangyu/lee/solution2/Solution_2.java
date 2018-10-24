ba373dbd58b8e48329383351b1ba13fb83c6f520
lixiangyu <lixiangyu1991@gmail.com>
2018-10-24 20:20:43 +0800

    [ADD]增加了solution2

---
+++ src/main/java/com/lixiangyu/lee/solution2/Solution_2.java
@@ -0,0 +1,42 @@
+package com.lixiangyu.lee.solution2;
+
+/**
+ * @program: leecode
+ * @description: leecode_2
+ * @author: lixiangyu
+ * @create: 2018-10-22 17:56
+ **/
+public class Solution_2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode list = new ListNode(0);
+        ListNode head = list;
+        int sum = 0;
+        while (l1 != null || l2 != null || sum != 0) {
+
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            list.next = new ListNode(sum % 10);
+            sum = sum / 10;
+            list = list.next;
+
+        }
+
+        return head.next;
+
+
+    }
+
+    public static void main(String[] args) {
+    }
+
+
+}
